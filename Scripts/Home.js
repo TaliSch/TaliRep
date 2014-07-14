dd46291a643ced159e6ef68fe9198775f34a94e2
TaliSch <tali.schmeidler@gmail.com>
2014-07-14 20:38:45 +0300

    add links to text

--- Scripts/Home.js
+++ Scripts/Home.js
@@ -1,13 +1,9 @@
 ﻿$().ready(function () {
-    var datas = GetNextItems(0, 10);
-    if (datas) {
-        for (var i = 0; i < datas.length; i++) {
-            $(posts).append("<p>" + datas[i] + "<p>");
-        }
-    }
+    loadNextItems(0, 10);
+    $a = $("#posts").find('a');   
 })
-function GetNextItems(from, to) {
-    var retval =[];
+function loadNextItems(from, to) {
+  
     $.ajax({
         url: 'Home/Index',
         type: 'POST',
@@ -21,17 +17,86 @@ function GetNextItems(from, to) {
          
             for (var i = 0; i < data.length; i++) {
                 // alert(data[i].Data);
-                
-                retval.push(LZString.decompressFromBase64(data[i].Data));
-                // alert(retval[i]);
+                var post = LZString.decompressFromBase64(data[i].Data);
+               
+                var maxHeight = calculateHeigth("<p>22</p>");               
+               
+                var lines = post.split("</p>", 1000);
+           
+                var parts = splitPost(lines, maxHeight);
+                //alert(parts);
+                var partsHtml = "";
+                var links = "";
+                var postId = "post" + i.toString();
+                $.each(parts, function (index, value) {
+                    var divId = "divId" + index.toString();
+                    var div = '<div id="' + divId + '">' + value + '</div>';
+                  
+                    partsHtml = partsHtml.concat(div);
+                    if (parts.length > 1) {                        
+                        links = links.concat('<a href="#" onclick="choosePage(' + '\'' + divId + '\'' + ',' + '\'' + postId + '\'' + ');return false;">' + index + '</a> ');
+                    }
+                });
+             
+                $("#posts").append('<table class="post" id="' + postId + '"<tr><td>' + partsHtml + links + '</td></tr></table>');
+                choosePage('divId0', postId);
+               
             }
-            
+          
         },
         error: function (jqXHR, textStatus, errorThrown) {
             alert("Failed To Load");
+            
         }
     });
-    alert(retval.length);
-    return retval;
-}
 
+    
+    function splitPost(lines, maxHeight) {
+        var parts = [];
+              
+        var currentPart = "";
+        var fromIndex = 0;
+        for (var j = 0; j < lines.length-1 ; j++) {         
+            currentPart = currentPart + lines[j] + "</p>";
+            
+            var currHeight = calculateHeigth(currentPart);
+            //alert(currHeight);
+            if (currHeight > maxHeight) {
+                currentPart = "";
+               // alert('fromIndex=' + fromIndex + ',j=' + j);
+                if (fromIndex < j) {
+                    for (var k = fromIndex; k < j; k++) {
+                        currentPart = currentPart.concat(lines[k] + "</p>");
+                    }
+                    parts[parts.length] = currentPart;
+                    fromIndex = j;
+                }
+                                
+                currentPart = lines[j] + "</p>";
+            }
+            
+            // alert(lines[j]);
+            // create div per part, with hide
+        }
+        if (currentPart != "") {
+            parts.push(currentPart);
+        }
+
+        return parts;
+    }
+
+    function calculateHeigth(currentPart) {
+       var $table = $("#parts");
+       var html = '<table class = "post" id="part"<tr><td>' + currentPart + '</td></tr></table>';
+       $table.append(html);
+       var currHeight = $table.find("#part").height();
+       $table.html("");
+       return currHeight;
+    }
+}
+function choosePage(divId, postId) {    
+    var $post = $('#posts').find('#' + postId);
+ 
+    $post.find('div').hide();
+    $post.find('#' + divId).show();
+}
