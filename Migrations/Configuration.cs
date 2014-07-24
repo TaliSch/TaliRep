4963435e089948b7611d146a0b01cf22992fa37e
TaliSch <tali.schmeidler@gmail.com>
2014-07-24 12:21:06 +0300

    fix migration nug

--- Migrations/Configuration.cs
+++ Migrations/Configuration.cs
@@ -10,6 +10,7 @@ namespace MyBlogEmpty.Migrations
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
         }
 
         protected override void Seed(MyBlogEmpty.Models.BlogDBContext context)
